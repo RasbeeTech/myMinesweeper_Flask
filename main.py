972511c233769efdff7fab59d48622a56f94cd65
MykalB081 <mykalb081@gmail.com>
2021-03-17 13:38:42 -0400

    update

--- main.py
+++ main.py
@@ -6,25 +6,18 @@ app = Flask(__name__)
 game = Minesweeper('medium')
 
 
-def play_field(difficulty):
-    if difficulty == 'easy':
-        return list(range(0, 6))
-    if difficulty == 'medium':
-        return list(range(0, 9))
-    if difficulty == 'hard':
-        return list(range(0, 12))
-
-
 @app.route('/')
 def index():
     game.change_difficulty('medium')
-    rows = columns = play_field('medium')
-    return render_template('index.html', rows=rows, columns=columns, difficulty='medium', num_of_flags=game.mines)
+    rows = columns = game.play_field()
+    return render_template('index.html',
+                           rows=rows, columns=columns,
+                           difficulty='medium',
+                           num_of_flags=game.mines)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def form_post():
-    # TODO: Set number of flags remaining
     # TODO: render template partially
     if request.method == 'POST':
         keys = []
@@ -32,8 +25,12 @@ def form_post():
             keys.append(key)
         if 'difficulty' in keys:
             game.change_difficulty(request.form['difficulty'])
-            rows = columns = play_field(request.form['difficulty'])
-            return render_template('index.html', rows=rows, columns=columns, difficulty=request.form['difficulty'])
+            rows = columns = game.play_field()
+            return render_template('index.html',
+                                   rows=rows,
+                                   columns=columns,
+                                   difficulty=game.difficulty,
+                                   num_of_flags=game.mines)
         if 'tile' in keys:
             print(request.form['tile'])
             return '', 204  # HTTP empty response
--- source.py
+++ source.py
@@ -1,10 +1,11 @@
-from random import randrange
+import random
 
 
 class Minesweeper:
     def __init__(self, difficulty):
         self.difficulty = difficulty
         self.mines = self.num_of_mines()
+        self.mine_locations = self.set_mines()
 
     def num_of_mines(self):
         return {
@@ -16,9 +17,35 @@ class Minesweeper:
     def change_difficulty(self, new_difficulty):
         self.difficulty = new_difficulty
         self.mines = self.num_of_mines()
+        self.mine_locations = self.set_mines()
+
+    def play_field(self):
+        if self.difficulty == 'easy':
+            return list(range(0, 6))
+        if self.difficulty == 'medium':
+            return list(range(0, 9))
+        if self.difficulty == 'hard':
+            return list(range(0, 12))
+
+    def set_mines(self):
+        num_of_bombs = self.mines
+        bomb_tiles = []
+        bombs_planted = 0
+
+        while bombs_planted < num_of_bombs:
+            row = random.choice(self.play_field())
+            col = random.choice(self.play_field())
+            if [row, col] not in bomb_tiles:
+                bomb_tiles.append([row, col])
+                bombs_planted += 1
+        return bomb_tiles
 
 
 if __name__ == '__main__':
     print('Minesweeper')
     game = Minesweeper('medium')
     print(game.num_of_mines())
+    print(game.play_field())
+    print(game.mine_locations)
+    print(game.set_mines())
+    print(game.mine_locations)
