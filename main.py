ada8cfb2c42510b8067fc111d9e393e570fa4696
MykalB081 <mykalb081@gmail.com>
2021-03-24 11:57:16 -0400

    update
    - Code clean-up
    - Implementing some JavaScript

--- main.py
+++ main.py
@@ -25,22 +25,24 @@ indicator_style = {
 }
 
 
+def templating(the_game):
+    rows = columns = the_game.play_field()
+    return render_template('index.html',
+                           rows=rows, columns=columns,
+                           difficulty='medium',
+                           num_of_flags=the_game.mines,
+                           mine_locations=the_game.mine_locations,
+                           ind_locations=the_game.ind_location,
+                           ind_number=the_game.ind_number,
+                           indicator_style=indicator_style,
+                           revealed_tiles=the_game.revealed_tiles)
+
+
 # TODO: render part of template instead of entire window
 #  https://stackoverflow.com/questions/21515554/render-part-of-the-template-in-flask
 @app.route('/')
 def index():
-    game.change_difficulty('medium')
-    rows = columns = game.play_field()
-    # TODO: create function to simplify rendering template:
-    return render_template('index.html',
-                           rows=rows, columns=columns,
-                           difficulty='medium',
-                           num_of_flags=game.mines,
-                           mine_locations=game.mine_locations,
-                           ind_locations=game.ind_location,
-                           ind_number=game.ind_number,
-                           indicator_style=indicator_style,
-                           revealed_tiles=game.revealed_tiles)
+    return templating(game)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -51,16 +53,7 @@ def form_post():
             keys.append(key)
         if 'difficulty' in keys:
             game.change_difficulty(request.form['difficulty'])
-            rows = columns = game.play_field()
-            return render_template('index.html',
-                                   rows=rows, columns=columns,
-                                   difficulty=game.difficulty,
-                                   num_of_flags=game.flags,
-                                   mine_locations=game.mine_locations,
-                                   ind_locations=game.ind_location,
-                                   ind_number=game.ind_number,
-                                   indicator_style=indicator_style,
-                                   revealed_tiles=game.revealed_tiles)
+            return templating(game)
         if 'tile' in keys:
             tile = request.form['tile']
             print(tile)
@@ -68,32 +61,12 @@ def form_post():
             game.reveal_tiles(location[0], location[1])
             # print(game.mine_locations)
             # print(game.revealed_tiles)
-            rows = columns = game.play_field()
-            return render_template('index.html',
-                                   rows=rows,
-                                   columns=columns,
-                                   difficulty=game.difficulty,
-                                   num_of_flags=game.flags,
-                                   mine_locations=game.mine_locations,
-                                   ind_locations=game.ind_location,
-                                   ind_number=game.ind_number,
-                                   indicator_style=indicator_style,
-                                   revealed_tiles=game.revealed_tiles)
-            # return '', 204  # HTTP empty response
+            return templating(game)
         if 'start_game' in keys:
             tile = request.form['start_game']
             location = [int(x) for x in tile.split()]
             game.start_game(location[0], location[1])
-            rows = columns = game.play_field()
-            return render_template('index.html',
-                                   rows=rows, columns=columns,
-                                   difficulty=game.difficulty,
-                                   num_of_flags=game.flags,
-                                   mine_locations=game.mine_locations,
-                                   ind_locations=game.ind_location,
-                                   ind_number=game.ind_number,
-                                   indicator_style=indicator_style,
-                                   revealed_tiles=game.revealed_tiles)
+            return templating(game)
         if 'mine_tile' in keys:
             game.toggle_game_over()
             return '', 204  # HTTP empty response
@@ -101,13 +74,4 @@ def form_post():
             # TODO: flask controlled Modal:
             #  https://stackoverflow.com/questions/54524827/how-to-show-bootstrap-modal-on-rendering-the-same-page-in-flask
             game.new_game()
-            rows = columns = game.play_field()
-            return render_template('index.html',
-                                   rows=rows, columns=columns,
-                                   difficulty=game.difficulty,
-                                   num_of_flags=game.flags,
-                                   mine_locations=game.mine_locations,
-                                   ind_locations=game.ind_location,
-                                   ind_number=game.ind_number,
-                                   indicator_style=indicator_style,
-                                   revealed_tiles=game.revealed_tiles)
+            return templating(game)
